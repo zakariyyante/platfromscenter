6f29b3b893cf344d2e60e971dee3fd900d17c7d5
Zakariyya <zakariyya.z@antetechnologies.com>
2026-09-12 09:59:47 +0400

    hdjshckdsjkcjdskjcd

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -10,36 +10,61 @@ export interface Brand {
 }
 
 export const brands: Brand[] = [
-//     {
-//     id: "TowerCasino",
-//     name: "TowerCasino",
-//     logo: "/brands/tower.png",
-//     rating: 9.9,
-//     bonus: "100% JUSQU'À 2000€ + 300TG",
-//     url: "https://twcs.joklora.com/?mid=372329_2243631&subid=TowerCasino-SL2FR35&clickid=",
-//     isMobile: true,
-//     votes: 12450,
-//   },
-//   {
-//         id: "LuckyGem",
-//         name: "LuckyGem",
-//         logo: "/brands/lucky.svg",
-//         rating: 9.9,
-//         bonus: "700% + 700TG",
-//         url: "https://luckygemplay.com/tmorahfie?subid=LuckyGem-SL2FR35&visit_id=",
-//         isMobile: true,
-//         votes: 12450,
-//       },
+    {
+    id: "TowerCasino",
+    name: "TowerCasino",
+    logo: "/brands/tower.png",
+    rating: 9.9,
+    bonus: "100% JUSQU'À 2000€ + 300TG",
+    url: "https://twcs.joklora.com/?mid=372329_2243631&subid=TowerCasino-SL2FR35&clickid=",
+    isMobile: true,
+    votes: 12450,
+  },
+  {
+        id: "LuckyGem",
+        name: "LuckyGem",
+        logo: "/brands/lucky.svg",
+        rating: 9.9,
+        bonus: "700% + 700TG",
+        url: "https://luckygemplay.com/tmorahfie?subid=LuckyGem-SL2FR35&visit_id=",
+        isMobile: true,
+        votes: 12450,
+      },
+  {
+    id: "VillaSpin",
+    name: "VillaSpin",
+    logo: "/brands/villaspin.webp",
+    rating: 9.8,
+    bonus: "500% JUSQU'À 15000€ + 500TG",
+    url: "https://go.drctvoodoo.com/click?pid=6&offer_id=731&sub_id1=VillaSpin-SL2FR35&aff_click_id=",
+    isMobile: true,
+    votes: 12450,
+    
+  },
+
 //     {
 //     id: "BetBluff",
 //     name: "BetBluff",
 //     logo: "/brands/BETBLUFF.svg",
 //     rating: 9.9,
-//     bonus: "350% JUSQU'À 2000€ & 1000TG ",
+//     bonus: "350% JUSQU'À 2000€ + 1000TG ",
 //     url: "https://go.evoaffiliates.com/visit/?bta=38782&nci=6556&afp1=BetBluff-SL2FR35&afp=",
 //     isMobile: true,
 //     votes: 12450,
 //   },
+
+  {
+    id: "SpinFormula",
+    name: "SpinFormula",
+    logo: "/brands/Spinformula.svg",
+    rating: 9.8,
+    bonus: "550% JUSQU'À 15000€ + 550TG",
+    url: "https://corsa.cxclick.com/visit/?bta=35059&nci=5345&utm_campaign=SpinFormula-SL2FR35&afp=",
+    isMobile: true,
+    votes: 12450,
+    
+    
+  }, 
 //   {
 //       id: "RoyalDogs",
 //       name: "RoyalDogs",
@@ -51,16 +76,16 @@ export const brands: Brand[] = [
 //       votes: 12450,
 //   },
   
-  {
-    id: "bwin-fr",
-    name: "Bwin",
-    logo: "/brands/bwin_dark.png",
-    rating: 9.9,
-    bonus: "Votre mise remboursée jusqu'à 100€",
-    url: "https://mediaserver.entainpartners.com/renderBanner.do?zoneId=2159573&clickid=",
-    isMobile: false,
-    votes: 2556,
-  },
+  // {
+  //   id: "bwin-fr",
+  //   name: "Bwin",
+  //   logo: "/brands/bwin_dark.png",
+  //   rating: 9.9,
+  //   bonus: "Votre mise remboursée jusqu'à 100€",
+  //   url: "https://mediaserver.entainpartners.com/renderBanner.do?zoneId=2159573&clickid=",
+  //   isMobile: false,
+  //   votes: 2556,
+  // },
   
 //   {
 //     id: "WinoCasino",
@@ -72,29 +97,8 @@ export const brands: Brand[] = [
 //     isMobile: true,
 //     votes: 12450,
 //   },
-//   {
-//     id: "SlotRush",
-//     name: "SlotRush",
-//     logo: "/brands/SLOTRUSH.png",
-//     rating: 9.8,
-//     bonus: "Jusqu'à 4000€ + 200FS",
-//     url: "https://linkslotrush.com/iyhqswoke?subid=SlotRush-SL2FR24&clickid=",
-//     isMobile: true,
-//     votes: 12450,
-    
-//   },
-//   {
-//     id: "SpinFormula",
-//     name: "SpinFormula",
-//     logo: "/brands/Spinformula.svg",
-//     rating: 9.8,
-//     bonus: "550% jusqu’à 15000€ + 550FS",
-//     url: "https://corsa.cxclick.com/visit/?bta=35059&nci=5345&utm_campaign=SpinFormula-SL2FR24&afp=",
-//     isMobile: true,
-//     votes: 12450,
-    
-    
-//   }, 
+
+
 //   {
 //     id: "BravoPlay",
 //     name: "BravoPlay",
