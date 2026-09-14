6bfd6a487261f93baab2db1a16bd9863b64708b7
Zakariyya <zakariyya.z@antetechnologies.com>
2026-09-14 21:24:44 +0400

    hdjshckdsjkcjdskjcd

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -115,7 +115,7 @@ export const brands: Brand[] = [
     logo: "/brands/bravoplay.svg",
     rating: 9.8,
     bonus: "Jusqu'à 5000€ + 150FS",
-    url: "https://trk.arztrc.site/click?o=1&a=82&c=314&link_id=39&sub_id1=BravoPlay-SL2FR24&aff_click_id=",
+    url: "https://trk.arztrc.site/click?o=1&a=82&c=314&link_id=39&sub_id1=BravoPlay-SL2FR35&aff_click_id=",
     isMobile: true,
     votes: 12450,
     
