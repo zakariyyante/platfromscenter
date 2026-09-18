554a8c8e768825a906545938f10c7a5c21ba80cb
Zakariyya <zakariyya.z@antetechnologies.com>
2026-09-18 10:54:33 +0400

    hdjshckdsjkcjdskjcd

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -11,7 +11,16 @@ export interface Brand {
 
 export const brands: Brand[] = [
  
-
+  {
+      id: "Lussurio",
+      name: "Lussurio",
+      logo: "/brands/lussurio.png",
+      rating: 9.9,
+      bonus: "550% JUSQU'À 3485€ + 400TG",
+      url: "https://lussurios.com/afweyuz2fh?subid=Lussurio-SL2FR35&visit_id=",
+    isMobile: true,
+    votes: 12450,
+  },
 
 {
   id: "SpinFormula",
